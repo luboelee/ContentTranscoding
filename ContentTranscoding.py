6c6697df0d1cc2facc0ab71d7ca02207ecefe8ae
KyungIl Lee <luboe.lee@gmail.com>
2026-04-28 21:48:13 +0900

    More implement

--- ContentTranscoding.py
+++ ContentTranscoding.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 from pathlib import Path
 import numpy as np
+import shutil
 
 
 TARGET_EXTENSION = [".mp4"]
@@ -11,16 +12,26 @@ FFPROBE = "ffprobe"
 USING_CUDA = True
 VIDEO_ENCODER = "hevc_nvenc"
 
-THRESHOLD_PSNR = 40.0
+THRESHOLD_PSNR = 42.0
 THRESHOLD_SSIM = 0.93
 COMPRESS_RATIO = [0.6, 0.7, 0.8, 0.9]
 
 class ContentTranscoding:
     def __init__(self, args):
         self.args = args
-        self.tmp_file = "_tmp.mp4"
+        self.temp_path = None
+        self.done_path = None
         
-    def gethering_target_files(self):
+    def __prepare(self, target_path):
+        self.temp_path = Path(target_path) / "temporary"
+        if os.path.isdir(self.temp_path) == False:
+            os.mkdir(self.temp_path)
+
+        self.done_path = Path(target_path) / "done"
+        if os.path.isdir(self.done_path) == False:
+            os.mkdir(self.done_path)
+
+    def __gethering_target_files(self):
         target_path = Path(self.args.path)
         target_files = []
         for ext in TARGET_EXTENSION:
@@ -37,23 +48,38 @@ class ContentTranscoding:
             video_encoder = "libx265"
             using_hwaccel = ""
 
-        cmd = f"{FFMPEG} -y {using_hwaccel} -i {target_file} -b:v {video_bitrate} -c:v {video_encoder} -c:a copy {self.tmp_file}"
+        transcoded_file = self.temp_path / target_file.name
+        cmd = f"{FFMPEG} -y -loglevel error {using_hwaccel} -i {target_file} -b:v {video_bitrate} -c:v {video_encoder} -c:a copy {transcoded_file}"
         try:
             subprocess.call(cmd, shell=True)
         except subprocess.CalledProcessError:
             print(f"[Error] Failed to transcode the video file = {target_file} and bitrate = {video_bitrate}")
+            transcoded_file = None
+        return transcoded_file
 
     def __measuring(self, anchor_file, target_file):
-        cmd = f'{FFMPEG} -i {anchor_file} -i {target_file} -filter_complex \"[1:v:0]split=2[ref1][ref2];[0:v:0][ref1]psnr=f=psnr_report.txt[v_pass];[v_pass][ref2]ssim=f=ssim_report.txt\" -f null -'
+        if target_file == None:
+            return
+
+        psnr_report = f"{target_file.name}_psnr.txt"
+        ssim_report = f"{target_file.name}_ssim.txt"
+        cmd = f'{FFMPEG} -loglevel error -i {anchor_file} -i {target_file} -filter_complex \"[1:v:0]split=2[ref1][ref2];[0:v:0][ref1]psnr=f={str(psnr_report)}[v_pass];[v_pass][ref2]ssim=f={str(ssim_report)}\" -f null -'
         try:
             subprocess.call(cmd, shell=True)
+            moved_psnr_path_file = self.temp_path / psnr_report
+            moved_ssim_path_file = self.temp_path / ssim_report
+            shutil.move(psnr_report, moved_psnr_path_file)
+            shutil.move(ssim_report, moved_ssim_path_file)
         except subprocess.CalledProcessError:
             print(f"[Error] Failed to measure the video file = {target_file}")
+            moved_psnr_path_file = None
+            moved_ssim_path_file = None
+        return moved_psnr_path_file, moved_ssim_path_file
 
-    def __parsing_psnr_ssim(self):
+    def __parsing_psnr_ssim(self, psnr_report, ssim_report):
         try:
             parsed_data = []
-            with open('psnr_report.txt', 'r', encoding='utf-8') as file:
+            with open(psnr_report, 'r', encoding='utf-8') as file:
                 for line in file:
                     parts = line.strip().split()
                     
@@ -70,7 +96,13 @@ class ContentTranscoding:
                             'psnr_y': float(line_dict['psnr_y'])
                         })
 
-            with open('ssim_report.txt', 'r', encoding='utf-8') as file:
+            psnr_avg_list = [data['psnr_avg'] for data in parsed_data]
+            psnr_y_list = [data['psnr_y'] for data in parsed_data]
+            avg_psnr_avg = np.mean(psnr_avg_list)
+            avg_psnr_y = np.mean(psnr_y_list)
+
+            parsed_data.clear()
+            with open(ssim_report, 'r', encoding='utf-8') as file:
                 for line in file:
                     parts = line.strip().split()
                     
@@ -83,15 +115,10 @@ class ContentTranscoding:
                     if 'All' in line_dict and 'Y' in line_dict:
                         parsed_data.append({
                             'frame': int(line_dict.get('n', 0)),
-                            'All': float(line_dict['ssim_all']),
-                            'Y': float(line_dict['ssim_y'])
+                            'ssim_all': float(line_dict['All']),
+                            'ssim_y': float(line_dict['Y'])
                         })
 
-            psnr_avg_list = [data['psnr_avg'] for data in parsed_data]
-            psnr_y_list = [data['psnr_y'] for data in parsed_data]
-            avg_psnr_avg = np.mean(psnr_avg_list)
-            avg_psnr_y = np.mean(psnr_y_list)
-            
             ssim_all_list = [data['ssim_all'] for data in parsed_data]
             ssim_y_list = [data['ssim_y'] for data in parsed_data]
             avg_ssim_all = np.mean(ssim_all_list)
@@ -102,33 +129,44 @@ class ContentTranscoding:
         
         return avg_psnr_avg, avg_psnr_y, avg_ssim_all, avg_ssim_y
     
-    def run_transcoding(self, target_files):
+    def __run_transcoding(self, target_files):
         for cur_file in target_files:
             cmd_get_bitrate = f"{FFPROBE} -v error -show_entries format=bit_rate -of default=noprint_wrappers=1:nokey=1 {cur_file}"
             try:
                 orig_video_bitrate = subprocess.check_output(cmd_get_bitrate, shell=True).decode("utf-8").strip()
+                transcoding_done = False
                 for ratio in COMPRESS_RATIO:
                     video_bitrate = int(orig_video_bitrate) * ratio
-                    self.__transcoding(cur_file, video_bitrate)
-                    self.__measuring(cur_file, self.tmp_file)
-                    avg_psnr_avg, avg_psnr_y, avg_ssim_all, avg_ssim_y = self.__parsing_psnr_ssim()
+                    print(f"Transcoding the video file = {cur_file} and original bitrate = {orig_video_bitrate}, target bitrate = {video_bitrate}")
+                    transcoded_file = self.__transcoding(cur_file, video_bitrate)
+                    if transcoded_file == None:
+                        continue
+                    psnr_report, ssim_report = self.__measuring(cur_file, transcoded_file)
+                    avg_psnr_avg, avg_psnr_y, avg_ssim_all, avg_ssim_y = self.__parsing_psnr_ssim(psnr_report, ssim_report)
                     if avg_psnr_avg > THRESHOLD_PSNR and avg_psnr_y > THRESHOLD_PSNR and avg_ssim_all > THRESHOLD_SSIM and avg_ssim_y > THRESHOLD_SSIM:
+                        print(f"[✔] Done transcoding. Avg PSNR: {avg_psnr_avg}, Avg PSNR Y: {avg_psnr_y}, Avg SSIM All: {avg_ssim_all}, Avg SSIM Y: {avg_ssim_y}")
+                        transcoding_done = True
                         break
+                    else:
+                        print(f"[✘] Substandard video quality: Avg PSNR: {avg_psnr_avg}/{THRESHOLD_PSNR}, Avg PSNR Y: {avg_psnr_y}/{THRESHOLD_PSNR}, Avg SSIM All: {avg_ssim_all}/{THRESHOLD_SSIM}, Avg SSIM Y: {avg_ssim_y}/{THRESHOLD_SSIM}")
+                if transcoding_done == False:
+                    transcoded_file.unlink(missing_ok=True)
+                    psnr_report.unlink(missing_ok=True)
+                    ssim_report.unlink(missing_ok=True)
                     
             except subprocess.CalledProcessError:
                 print("[Error] Failed to get bitrate of the video file = {}".format(cur_file))
             
     def run(self):
-        target_files = self.gethering_target_files()
-        self.run_transcoding(target_files)
-
+        self.__prepare(self.args.path)
+        target_files = self.__gethering_target_files()
+        self.__run_transcoding(target_files)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--path", required=True, help="Path to the video file")
-    
     args = parser.parse_args()
-    
+
     content_transcoding = ContentTranscoding(args)
     content_transcoding.run()
